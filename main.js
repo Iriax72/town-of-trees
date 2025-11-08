6e5628142159b8bbea34dd954431b6bf6a21abab
flexult <touma.leandre.72@gmail.com>
2025-11-08 16:12:09 +0000

    ajustement

--- main.js
+++ main.js
@@ -5,4 +5,10 @@ const engine = new BABYLON.Engine(canvas, true);
 const createScene = () => {
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(0.1, 0.3, 0.9);
+
+    return scene
 }
+
+const scene = createScene();
+
+engine.runRenderLoop(() => scene.render());
