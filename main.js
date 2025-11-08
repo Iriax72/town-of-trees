c5b106ae687a33165a35064f925b55c499e00dd0
flexult <touma.leandre.72@gmail.com>
2025-11-08 16:09:22 +0000

    création des fichiers et début de la scene

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#renderCanvas");
+
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = () => {
+    const scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(0.1, 0.3, 0.9);
+}
