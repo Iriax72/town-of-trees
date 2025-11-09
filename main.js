9cfb8c63f091b5fe6d4d9cd2c49c8443dc4fe3dc
flexult <touma.leandre.72@gmail.com>
2025-11-09 19:32:15 +0000

    test 2

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function createJoystick(ui) {
     joystickBase.thickness = 0;
     joystickBase.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
     joystickBase.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
-    //joystickBase.right = "70px";
+    joystickBase.right = "70px";
     joystickBase.bottom = "70px";
     joystickBase.alpha = 0.4;
     ui.addControl(joystickBase);
